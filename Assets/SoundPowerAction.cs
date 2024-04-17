9c8e93382305b217bf7bad6683db41d8bed8365d
PhilippeCheng <philippe8910@gmail.com>
2024-04-17 22:07:34 +0800

    Fix Bug

--- Assets/SoundPowerAction.cs
+++ Assets/SoundPowerAction.cs
@@ -147,7 +147,7 @@ public class SoundPowerAction : MonoBehaviour
             {
                 float waitForSeconds = 1.5f - Mathf.Clamp((loudness - 0.5f) / 10f , -100 , 100);
 
-                StartCoroutine(startCountdown(Mathf.Clamp(waitForSeconds , 0.001f , 100)));
+                StartCoroutine(startCountdown(Mathf.Clamp(waitForSeconds , 0.5f , 100)));
                 Debug.Log("No SoundPowerAction");
                 Debug.Log("waitForSeconds: " + waitForSeconds);
             }
